7fffaa2d6834ef2f133bd341b7f52319d92e0748
Reyer Swengel <reyerswengel@gmail.com>
2017-01-21 04:27:48 -0600

    Add all

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public string player;
+
+    float speed = 6f;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        var horizontal = "Horizontal_P" + player;
+        var vertical = "Vertical_P" + player;
+        float h = Input.GetAxisRaw(horizontal);
+        float v = Input.GetAxisRaw(vertical);
+
+        if(Input.GetAxisRaw(horizontal) > 0)
+        {
+            //Debug.Log(Input.GetAxisRaw(horizontal));
+        }
+
+        if (Input.GetButton("Button 1"))
+        {
+            Debug.Log(player);
+        }
+
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
